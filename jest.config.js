154dabaa2033eb6d6c7d5b71e8a59953ce8d889b
Igor <dev@example.com>
2026-05-25 16:40:13 +0300

    test: improve test log messages for better readability

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 // Конфигурация Jest для запуска unit-тестов на TypeScript.
 module.exports = {
   testEnvironment: 'node',
+  verbose: true,
   roots: ['<rootDir>/RPGSagaUnitTests'],
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
